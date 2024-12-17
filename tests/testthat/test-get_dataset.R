2e7dbe96049643eaff0016d4550c9fd8f1e05d68
Cédric Bobinec <cedric.bobinec@insee.fr>
2024-12-17 08:42:38 +0100

    enrichissement des tests

--- tests/testthat/test-get_dataset.R
+++ tests/testthat/test-get_dataset.R
@@ -7,13 +7,30 @@ test_that("get_dataset works", {
                          "44115")
 
     expect_is(res, "list")
-    expect_equal(as.character(res$source$jeu_donnees), "GEO2017REE2017")
+    expect_equal(as.character(res$source$code), "GEO2017REE2017")
     expect_equal(res$donnees$codgeo[1], "44115")
     expect_named(res, c("donnees", "liste_code", "info_zone", "source"))
     expect_is(res$donnees, "data.frame")
     expect_is(res$liste_code, "data.frame")
     expect_is(res$info_zone, "data.frame")
     expect_is(res$source, "data.frame")
-    expect_equal(dim(res$donnees), c(18, 7))
+    expect_equal(dim(res$donnees), c(18, 5))
+  })
+  vcr::use_cassette("popleg2020-com-44115", {
+    res <- get_dataset("popleg2020",
+                       "ind_poplegales",
+                       'all',
+                       "COM",
+                       "44109")
+
+    expect_is(res, "list")
+    expect_equal(as.character(res$source$code), "POPLEG2020")
+    expect_equal(res$donnees$codgeo[1], "44109")
+    expect_named(res, c("donnees", "liste_code", "info_zone", "source"))
+    expect_is(res$donnees, "data.frame")
+    expect_is(res$liste_code, "data.frame")
+    expect_is(res$info_zone, "data.frame")
+    expect_is(res$source, "data.frame")
+    expect_equal(dim(res$donnees), c(3, 4))
   })
 })
